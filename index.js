6a685446268ba83972a1b315f4ab80bb57b46a28
Vipin Sharma <67193569+Vipinsharma417@users.noreply.github.com>
2021-08-20 12:57:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+//console.log("Hello World");
+//alert("Me");
+//document.write("This is me How Are you");
+//console.warn("This is Warning");
+//onsole.error("This Is Error");
+
+// JavaScript Varibles
+var numbers1 = 3;
+var numbers2 = 4;
+//console.log(numbers1*numbers2);
+
+//Data Types in JavaScript
+//numbers
+var num1 = 44;
+var num2 = 32;
+
+//Strings
+var str1 = " This is a String";
+var str2 = " This is also a String";
+
+//objects
+var marks = {
+    ravi: 23,
+    shubam: 45,
+    harry: 99
+}
+//console.log(marks);
+
+//Boolean
+
+var a = true;
+var b = false;
+//console.log(a);
+
+//Array in Java Script
+
+var Array = [1, 2, 3, "Vipin_Sharma", 4, 5]
+//console.log(Array);
+
+// Operators in JavaScript
+//Arithmetic Operators
+var a = 199;
+var b = 200;
+// console.log("The value of a + b ", a + b);
+// console.log("The value of a - b ", a - b);
+// console.log("The value of a * b ", a * b);
+// console.log("The value of a / b ", a / b);
+
+//Function in JavaScript
+
+function avg(a, b) {
+    c = ((a + b) / 2);
+    return c;
+
+}
+c1 = avg(5, 5);
+c2 = avg(10, 10);
+
+//console.log(c1, c2);
+
+//Conditional in JavaScript
+
+// var age = 34;
+// if(age>8){
+//     console.log("You are not a kid");
+// }
+// else{
+//     console.log("You are a kid");
+// }
+
+//Lopping in JavaScript
+
+var arr = [1, 2, 3, 4, 5, 566, 6]
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (i == 2) {
+//         //break;
+//         continue;
+//     }
+//     console.log(arr[i]);
+// }
+
+//While Loop
+
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+// let j = 0;
+// while (j = arr.length) {
+//     console.log(arr[j]);
+//     j++;
+// }
+
+// Array Method's
+
+//let myArr = ["Fan", "Camera", 34, null, true]
+//console.log(myArr.length);
+//myArr.pop(34);
+//console.log(myArr.pop(35));
+//console.log(myArr.length);
+//console.log(myArr.push("Vipin_Sharma"));
+//console.log(myArr);
+// console.log(myArr.shift("Camera"));
+// console.log(myArr)
+// console.log(myArr.unshift("Harry"));
+// console.log(myArr);
+// const newLen = myArr.unshift("Harry");
+// console.log(newLen);
+
+// String Method's
+
+// let myLoveString = "Vipin Sharma is a good Boy";
+// console.log(myLoveString.length);
+// console.log(myLoveString.indexOf("Sharma"));
+// console.log(myLoveString.lastIndexOf("good"));
+// console.log(myLoveString.endsWith("good"));
+// console.log(myLoveString.slice(1, 8));
+// console.log(myLoveString.replace("Vipin", "Abhishek"));
+
+//JavaScript Date's
+
+// myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getTime());
+// console.log(myDate.getDay());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getHours());
+
+// Dom Manipulation
